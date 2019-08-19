110c365fc695f704a86db3b4ee65ccb8cbabfd4b
Andrew Asplund <andrew.j.asplund@gmail.com>
2019-08-19 01:21:12 -0700

    Ugg.

---
+++ js/api.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+export let firebaseConfig = {
+    apiKey: "AIzaSyBMDRXSa_D27uFABQr0KA-rRVSd8ZOdIA0",
+    authDomain: "profounddark-capstone.firebaseapp.com",
+    databaseURL: "https://profounddark-capstone.firebaseio.com",
+    projectId: "profounddark-capstone",
+    storageBucket: "profounddark-capstone.appspot.com",
+    messagingSenderId: "433989331653",
+    appId: "1:433989331653:web:3a173da334de7223"
+  };
